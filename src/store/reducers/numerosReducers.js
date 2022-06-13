09afd2e53d558fb575c908d68d32109936e21fd4
CaioCDJ <caiodjesus1@hotmail.com>
2022-06-13 16:47:50 -0300

    ActionType

--- src/store/reducers/numerosReducers.js
+++ src/store/reducers/numerosReducers.js
@@ -1,3 +1,5 @@
+import { NUM_MAX_ALTERADO, NUM_MIN_ALTERADO } from "../actions/actionsTypes";
+
 const initialState={
     min:1,
     max:10
@@ -6,13 +8,13 @@ const initialState={
 export default function(state=initialState,action){
         
         switch(action.type){
-            case 'NUM_MIN_ALTERADO':
+            case NUM_MIN_ALTERADO:
                 return{
                     ...state,
                     min:action.payload
                 }
             break;
-            case 'NUM_MAX_ALTERADO':
+            case NUM_MAX_ALTERADO:
                 return{
                     ...state,
                     max:action.payload
